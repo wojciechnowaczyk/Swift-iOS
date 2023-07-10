6e98eb6a3540cf6fbe3e0113bee6c9a21d3a437d
wojciech.nowaczyk <wojciech.nowaczyk@chrobinson.com>
2023-07-10 17:35:32 +0200

    project init

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by wojciech.nowaczyk on 10/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
