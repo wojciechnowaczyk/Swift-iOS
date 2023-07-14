3fc804017c18d292812a803e3454dff610db0c8f
wojciech.nowaczyk <wojciech.nowaczyk@chrobinson.com>
2023-07-14 16:41:36 +0200

    init project and UISlider configs

---
+++ BMICalculator/BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        let weight = String(format: "%.0f", sender.value)
+        heightLabel.text = "\(weight)Kg"
+    }
+}
+
