255f547d33a8491b070bc66849c180dafa8d551d
wojciech.nowaczyk <wojciech.nowaczyk@chrobinson.com>
2023-07-15 17:01:18 +0200

    Background colors and advices dynamically set up

--- BMICalculator/BMI Calculator/Controllers/CalculatorBrain.swift
+++ BMICalculator/BMI Calculator/Controllers/CalculatorBrain.swift
@@ -6,18 +6,35 @@
 //  Copyright © 2023 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmiValue : String = "0.0"
+    var bmi : BMI?
     
     mutating func calculateBMI(height: Float, weight: Float){
-        let bmi = weight / pow(height, 2.0)
-        bmiValue = String(format: "%.1f", bmi)
+        let bmiValue = weight / pow(height, 2.0)
+
+        if(bmiValue < 18.5){
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.blue)
+        }else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor.green)
+        }else{
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
+        }
+   
     }
     
     func getBMIValue() -> String {
-        return bmiValue
+        let bmiValueToDecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
+        return bmiValueToDecimalPlace
+    }
+    
+    func getColor() -> UIColor {
+        return bmi?.color ?? UIColor.white
+    }
+    
+    func getAdvice() -> String {
+        return bmi?.advice ?? ""
     }
 }
