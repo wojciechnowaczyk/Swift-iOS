a6c73b1ba8a8ff462555e1f724d02ddc247427f9
Wojciech Nowaczyk <wojciech.nowaczyk@gmail.com>
2023-09-16 15:07:04 +0200

    IQKeyboadManager was implemented by Swift Package Manager

--- Chat/Flash Chat iOS13/AppDelegate.swift
+++ Chat/Flash Chat iOS13/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import FirebaseCore
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        IQKeyboardManager.shared.enable = true //enables the IQKeyboardManagersSwift package
+        IQKeyboardManager.shared.enableAutoToolbar = false //disabling auto toolbar
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true // enabling closing keyobard when clicking outside the keyboard
         return true
     }
 
